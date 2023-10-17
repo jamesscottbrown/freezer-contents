9504b0624bfabe3d954258ae520f3dec69c7969c
James Scott-Brown <james@jamesscottbrown.com>
2023-10-17 22:16:48 +0100

    remove unused example code for file embedding

--- main.go
+++ main.go
@@ -8,46 +8,10 @@ import (
 	"io/fs"
 	"io/ioutil"
 	"net/http"
-	"os"
-	"path"
 )
 
 //go:embed ui/build/*
 var ui embed.FS
-
-//
-// fsFunc is short-hand for constructing a http.FileSystem
-// implementation
-type fsFunc func(name string) (fs.File, error)
-
-func (f fsFunc) Open(name string) (fs.File, error) {
-	return f(name)
-}
-
-// AssetHandler returns an http.Handler that will serve files from
-// the Assets embed.FS.  When locating a file, it will strip the given
-// prefix from the request and prepend the root to the filesystem
-// lookup: typical prefix might be /web/, and root would be build.
-func AssetHandler(prefix, root string) http.Handler {
-	handler := fsFunc(func(name string) (fs.File, error) {
-		assetPath := path.Join(root, name)
-
-		// If we can't find the asset, return the default index.html
-		// content
-		f, err := ui.Open(assetPath)
-		if os.IsNotExist(err) {
-			return ui.Open("build/index.html")
-		}
-
-		// Otherwise assume this is a legitimate request routed
-		// correctly
-		return f, err
-	})
-
-	return http.StripPrefix(prefix, http.FileServer(http.FS(handler)))
-}
-
-//
 
 func main() {
 
