09c73071cc619a58d9141ced0adbe2e325b527bf
James Scott-Brown <james@jamesscottbrown.com>
2026-01-27 09:11:00 +0000

    update vite config to allow preview without re-building backend

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,6 +1,28 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
+const apiProxy = {
+  target: "http://localhost:8080",
+  changeOrigin: true,
+};
+
 export default defineConfig({
   plugins: [sveltekit()],
+  server: {
+    proxy: {
+      "/state": apiProxy,
+      "/add": apiProxy,
+      "/remove": apiProxy,
+      "/move": apiProxy,
+    },
+  },
+  preview: {
+    proxy: {
+      "/state": apiProxy,
+      "/add": apiProxy,
+      "/remove": apiProxy,
+      "/move": apiProxy,
+    },
+    allowedHosts: true, // N.B. this is only safe is access to dev server is restricted by other means
+  },
 });
